27ebd343e71cfbeee0020c857bddfc86cc522d0c
Maciej Ratkiewicz <matsaki95@tutanota.com>
2023-04-30 17:28:21 +0300

    Initial commit - Spring Boot template

---
+++ src/main/java/com/erekrazou/deliveryrouterbackend/DeliveryRouterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.erekrazou.deliveryrouterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryRouterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryRouterBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/erekrazou/deliveryrouterbackend/DeliveryRouterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erekrazou.deliveryrouterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryRouterBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

7d5d75546fa59519f980c86f8ef0321dbcf65a4b
Maciej Ratkiewicz <matsaki95@tutanota.com>
2023-05-03 22:18:23 +0300

    Added proper points and lines to `theOne` service

--- src/main/java/com/erekrazou/deliveryrouterbackend/services/GeoServiceImplementation.java
+++ src/main/java/com/erekrazou/deliveryrouterbackend/services/GeoServiceImplementation.java
@@ -11,8 +11,44 @@ public class GeoServiceImplementation implements GeoService {
     @Override
     public ResponseEntity<Object> theOne() {
         Features features = new Features();
-        features.addFeature(new Point(41.09106505, 23.54979639));
-        features.addFeature(new Line(new Point(41.09106505, 23.54979639), new Point(40.90787715, 23.50035572)));
+
+        Point serres = new Point(41.092083, 23.541016);
+        Point provatas = new Point(41.068238, 23.390686);
+        Point aKamila = new Point(41.058320, 23.424134);
+        Point kKamila = new Point(41.020431, 23.483293);
+        Point kMitrousi = new Point(41.058680, 23.457547);
+        Point koumaria = new Point(41.016434, 23.434656);
+        Point skoutari = new Point(41.020032, 23.520701);
+        Point adelfiko = new Point(41.014645, 23.457354);
+        Point agEleni = new Point(41.003545, 23.559196);
+        Point peponia = new Point(40.988154, 23.516756);
+
+        features.addFeature(serres);
+        features.addFeature(provatas);
+        features.addFeature(aKamila);
+        features.addFeature(kKamila);
+        features.addFeature(kMitrousi);
+        features.addFeature(koumaria);
+        features.addFeature(skoutari);
+        features.addFeature(adelfiko);
+        features.addFeature(agEleni);
+        features.addFeature(peponia);
+
+        features.addFeature(new Line(serres, provatas));
+        features.addFeature(new Line(serres, kKamila));
+        features.addFeature(new Line(serres, skoutari));
+        features.addFeature(new Line(provatas, aKamila));
+        features.addFeature(new Line(kMitrousi, aKamila));
+        features.addFeature(new Line(kMitrousi, koumaria));
+        features.addFeature(new Line(kMitrousi, kKamila));
+        features.addFeature(new Line(skoutari, kKamila));
+        features.addFeature(new Line(skoutari, peponia));
+        features.addFeature(new Line(skoutari, agEleni));
+        features.addFeature(new Line(aKamila, koumaria));
+        features.addFeature(new Line(kKamila, koumaria));
+        features.addFeature(new Line(koumaria, adelfiko));
+        features.addFeature(new Line(peponia, adelfiko));
+        features.addFeature(new Line(peponia, agEleni));
 
         return ResponseEntity.ok(features.asGeoJson());
     }
